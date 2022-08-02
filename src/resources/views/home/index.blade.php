990d80e9652988cff1fe5ff0f2d3f998a5f3f65b
roar-ray <roar1215@gmail.com>
2022-08-02 21:42:11 +0900

    ヘッダー作成

---
+++ src/resources/views/home/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+<header id="header">
+  <h1 class="logo"><a href="#">Book Review</a></h1>
+  <nav>
+    <ul class="nav__list">
+      <li class="nav__item"><a href="#">HOME</a></li>
+      <li class="nav__item"><a href="#">BOOK</a></li>
+      <li class="nav__item"><a href="#">ABOUT</a></li>
+    </ul>
+  </nav>
+</header>
+  </body>
+</html>
