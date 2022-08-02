0ee6ecdd25f2abf469c4bd6601562712fd17d6f5
roar-ray <roar1215@gmail.com>
2022-08-02 20:52:55 +0900

    first commit

---
+++ src/database/migrations/2022_08_02_114101_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('isbn',13);
+            $table->date('read_start_date');
+            $table->date('read_end_date');
+            $table->integer('current_page')->unsigned();
+            $table->integer('rate')->unsigned();
+            $table->string('comment');
+            $table->boolean('is_open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
